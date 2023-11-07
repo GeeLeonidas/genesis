e88e2e33701ce91457d951a6f2dc4b77562781f6
Guilherme Leoi <geeleonidas@pm.me>
2023-11-07 20:16:33 -0300

    Use tuple table syntax

--- tests/tester.nim
+++ tests/tester.nim
@@ -8,23 +8,37 @@ suite "Gene Expression Programming":
   test "Define and evaluate arithmetic symbols":
     let
       def = initSymDef(
-        binaryOps = [
-          ("Add", newBinaryOp(x + y)),
-          ("Sub", newBinaryOp(x - y)),
-          ("Mul", newBinaryOp(x * y)),
-          ("Div", newBinaryOp(x / y))
-        ],
-        terminalIdxs = [
-          ("a", 0),
-          ("b", 1),
-          ("c", 2)
-        ]
+        binaryOps = {
+          "Add": newBinaryOp(x + y),
+          "Sub": newBinaryOp(x - y),
+          "Mul": newBinaryOp(x * y),
+          "Div": newBinaryOp(x / y)
+        },
+        terminalIdxs = {
+          "a": 0,
+          "b": 1,
+          "c": 2
+        }
       )
       geneOne = def.fromNamesToGene(["Mul", "Add", "a", "b", "c"])
       geneTwo = def.fromNamesToGene(["Div", "Add", "Mul", "Sub", "a", "b", "c", "b", "a"])
-    check def.prefixEval(geneOne, [1.0, 2.0, 3.0])[0] ==  9.0
-    check def.prefixEval(geneOne, [3.0, 2.0, 1.0])[0] ==  5.0
-    check def.prefixEval(geneOne, [2.0, 0.0, 2.0])[0] ==  4.0
-    check def.prefixEval(geneTwo, [1.0, 2.0, 3.0])[0] == -1.0
-    check def.prefixEval(geneTwo, [3.0, 2.0, 1.0])[0] ==  1.0
-    check def.prefixEval(geneTwo, [2.0, 0.0, 2.0])[0] ==  2.0
+      checks = {
+        geneOne: {
+          def.prefixEval(geneOne, [1.0, 2.0, 3.0]):  9.0,
+          def.prefixEval(geneOne, [3.0, 2.0, 1.0]):  5.0,
+          def.prefixEval(geneOne, [2.0, 0.0, 2.0]):  4.0
+        },
+        geneTwo: {
+          def.prefixEval(geneTwo, [1.0, 2.0, 3.0]): -1.0,
+          def.prefixEval(geneTwo, [3.0, 2.0, 1.0]):  1.0,
+          def.prefixEval(geneTwo, [2.0, 0.0, 2.0]):  2.0
+        }
+      }
+    for checkGroup in checks:
+      let (gene, evalTable) = checkGroup
+      for row in evalTable:
+        let
+          (evalInfo, expected) = row
+          (evaluated, lastIdx) = evalInfo
+        check evaluated == expected
+        check lastIdx   == gene.high
